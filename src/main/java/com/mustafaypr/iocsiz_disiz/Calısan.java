7b6f68d0fad922deb0fe9c54d9c0ba0af0bc39cd
mustafay <mustafa_yapar99@hotmail.com>
2025-01-28 12:09:30 +0300

    iocsiz_disiz

---
+++ src/main/java/com/mustafaypr/iocsiz_disiz/Calısan.java
@@ -0,0 +1,14 @@
+package com.mustafaypr.iocsiz_disiz;
+
+public class Calısan {
+
+    public Patron patron;
+
+    public Calısan(){
+        patron = new Patron();
+    }
+    public String getData(String data){
+
+        return patron.Surum("23");
+    }
+}
---
+++ src/main/java/com/mustafaypr/iocsiz_disiz/Patron.java
@@ -0,0 +1,9 @@
+package com.mustafaypr.iocsiz_disiz;
+
+public class Patron {
+
+    public String Surum(String data) {
+
+        return "sürüm " + data;
+    }
+}
